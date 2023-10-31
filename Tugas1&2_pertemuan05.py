36d15ed96695c0e3adc48905eca15e0971db6d7c
Fadillahalfarouk <145969877+Fadillahalfarouk@users.noreply.github.com>
2023-10-31 14:32:25 +0700

    Tugas DDP pertemuan ke 05

---
+++ Tugas1&2_pertemuan05.py
@@ -0,0 +1,24 @@
+a = "Kode Kendaraan  :"
+b = "Nama Kendaraan  :"
+c = "Merek Kendaraan :"
+d = "Jenis Kendaraan :"
+e = "CC Kendaraan    :"
+f = "Warna Kendaraan :"
+g = "Harga Kendaraan :"
+h = "Ban Kendaraan   :"
+
+Kendaraan = ["D 3 POK", "CBR 1000RR", "1000", "Hitam"]
+
+Kendaraan.append("Rp 2.000.000.000,00")
+Kendaraan.append("2")
+Kendaraan.insert(2, "Honda")
+Kendaraan.insert(3,"Motor Kopling")
+
+print(a,Kendaraan[0])
+print(b,Kendaraan[1])
+print(c,Kendaraan[2])
+print(d,Kendaraan[3])
+print(e,Kendaraan[4])
+print(f,Kendaraan[5])
+print(g,Kendaraan[6])
+print(h,Kendaraan[7])
